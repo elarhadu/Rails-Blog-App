3567959cadbba1abaf4d24d6f092e658f371b7d6
elarhadu <elarhadu@gmail.com>
2023-04-08 00:17:38 +0000

    Add a comment association with the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', class_name: 'Post'
+  has_many :comments, foreign_key: 'user_id', class_name: 'Comment'
 
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
