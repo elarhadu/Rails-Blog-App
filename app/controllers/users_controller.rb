4f4b2ddcbb63a340938475522ff2d7253810b813
elarhadu <elarhadu@gmail.com>
2023-04-13 00:08:13 +0000

    Fix n+1 issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    @current_user = current_user
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(posts: [:comments]).find(params[:id])
+    @user_posts = @user.posts
+    @recent_posts = @user.recent_posts.includes(:comments)
   end
 end
